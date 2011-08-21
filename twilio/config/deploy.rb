32402889fec576b471b0b26654183fcbae1846a8
James Carr <james.r.carr@gmail.com>
2011-08-21 01:51:00 -0500

    modified build script

---
+++ twilio/config/deploy.rb
@@ -0,0 +1,79 @@
+set :stages, %w[staging production]
+set :default_stage, 'staging'
+require 'capistrano/ext/multistage'
+
+set :application, "twilio-spike"
+set :node_file, "app.js"
+set :host, "surety.me"
+set :repository, "git@github.com:jamescarr/sample-node-app.git"
+set :user, "node"
+set :admin_runner, 'node'
+set :application_binary, '/usr/local/bin/node'
+
+set :scm, :git
+set :deploy_via, :remote_cache
+role :app, host
+set :use_sudo, true
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "sudo start #{application}_#{node_env}"
+  end
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "sudo stop #{application}_#{node_env}"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "sudo restart #{application}_#{node_env} || sudo start #{application}_#{node_env}"
+  end
+
+  desc "Check required packages and install if packages are not installed"
+  task :update_packages, roles => :app do
+    run "cd #{release_path} && npm install"
+  end
+
+  task :create_deploy_to_with_sudo, :roles => :app do
+    run "sudo mkdir -p #{deploy_to}"
+    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
+  end
+
+  task :write_upstart_script, :roles => :app do
+    upstart_script = <<-UPSTART
+  description "#{application}"
+
+  start on startup
+  stop on shutdown
+
+  script
+      # We found $HOME is needed. Without it, we ran into problems
+      export HOME="/home/#{admin_runner}"
+      export NODE_ENV="#{node_env}"
+      cd #{current_path}
+
+      exec sudo -u #{admin_runner} sh -c "NODE_ENV=#{node_env} #{application_binary} #{current_path}/twilio/#{node_file} #{application_port} >> #{shared_path}/log/#{node_env}.log 2>&1"
+  end script
+  respawn
+UPSTART
+  put upstart_script, "/tmp/#{application}_upstart.conf"
+    run "sudo mv /tmp/#{application}_upstart.conf /etc/init/#{application}_#{node_env}.conf"
+  end
+
+  desc "Update submodules"
+  task :update_submodules, :roles => :app do
+    run "cd #{release_path}; git submodule init && git submodule update"
+  end
+
+  task :create_deploy_to_with_sudo, :roles => :app do
+    run "sudo mkdir -p #{deploy_to}"
+    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
+  end
+
+end
+
+
+before 'deploy:setup', 'deploy:create_deploy_to_with_sudo'
+after 'deploy:setup', 'deploy:write_upstart_script'
+after "deploy:finalize_update", "deploy:update_submodules", "deploy:update_packages", 
+
---
+++ twilio/spike.js
@@ -0,0 +1,19 @@
+var TwilioClient = require('twilio').Client,
+      Twiml = require('twilio').Twiml;
+
+var config = {
+   account_sid: 'AC97f3ce6e4134aef9b1927c4f72dc71f0'
+ , auth_token: '64da6fe27322bf04b64fdaa9b645b4b3'
+ , hostname: 'localhost'
+};
+var client = new TwilioClient(config.account_sid, config.auth_token, config.hostname);
+
+var phone = client.getPhoneNumber('+1(415)599-2671');
+phone.setup(function() { 
+  phone.makeCall('+15733554248', null, function(call) {
+    call.on('answered', function(callParams, response) {
+      response.append(new Twiml.Say("Hey buddy. Let's meet for drinks later tonight."));
+      response.send();
+    });
+  });
+});
